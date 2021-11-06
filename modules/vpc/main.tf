d806eecfbd1b0810565701f43bef5bfc94e22070
chris-carelli <50923511+chris-carelli@users.noreply.github.com>
2021-11-06 06:21:56 -0400

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -23,7 +23,7 @@ module "vpc" {
   subnets = [
     {
       subnet_name   = "${var.env}-subnet-01"
-      subnet_ip     = "10.${var.env == "dev" ? 10 : 84}.10.0/24"
+      subnet_ip     = "10.${var.env == "dev" ? 85 : 90}.10.0/24"
       subnet_region = "northamerica-northeast1"
     },
   ]
